55432628e385f667dc17c8bb73dbc60268d88f3c
Kanishka-verma-26 <kanishka@Hashstudioz.com>
2022-02-18 17:49:32 +0530

    serializers added

--- django/store/serializers.py
+++ django/store/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Product
+from .models import Product,ProductImage,Category
 
 """
  Serializer is a tool that can convert our complex data models into native Python datatypes.
@@ -9,7 +9,21 @@ from .models import Product
  (serializers allow us to format the collected data to send it across the frontend)
 """
 
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ["id","image","alt_text"]
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    product_image = ImageSerializer(many=True, read_only=True)          # through this we can also call the images of the product
+
     class Meta:
         model = Product
-        fields = ["id","title","description"]               # in fields we define data that we want to collect
+        fields = ["id","category",'slug',"title","description","regular_price","product_image"]               # in fields we define data that we want to collect
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["name", "slug"]
